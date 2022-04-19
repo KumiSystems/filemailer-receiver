1db908399ed178d024d3f46b8b2c76032a2d0835
Kumi <git@kumi.email>
2022-04-19 08:54:28 +0200

    Preparation for usage on vessel, pyinstaller

--- worker.py
+++ worker.py
@@ -4,6 +4,8 @@ from aiosmtpd.smtp import AuthResult
 import asyncio
 import logging
 
+from argparse import ArgumentParser
+
 from classes.smtpdhandler import SmtpdHandler
 from classes.config import Config
 from classes.authenticator import Authenticator
@@ -12,10 +14,14 @@ from classes.ssl import SSL
 if __name__ == "__main__":
     log = logging.basicConfig()
 
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", help="Path to config file", default="settings.ini")
+    args = parser.parse_args()
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    config = Config("settings.ini")
+    config = Config(args.config)
     authenticator = Authenticator(config)
     handler = SmtpdHandler(config)
 
